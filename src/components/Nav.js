a92e2e2f09fe1bb29540ece78d590757413384f4
Patrycja Miekina <patrycja.miekina@madetech.com>
2023-01-23 13:42:50 +0000

    add navbar styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,21 @@
+import { useNavigate } from 'react-router-dom'
+import logo from '../images/crm-logo.png'
+
 const Nav = () => {
+    // const navigate = useNavigate()
+    
     return (
-        <div>Nav</div>
+        <nav>
+            <div className='logo-container'>
+                <img src={logo} alt='logo' />
+            </div>
+            <div className='controls-container'>
+                {/* <div className='icon' onClick={() => navigate('/ticket')}>+</div>
+                <div className='icon' onClick={() => navigate('/')}>«</div> */}
+                <div className='icon'>+</div>
+                <div className='icon'>«</div>
+            </div>
+        </nav>
     )
 }
 
