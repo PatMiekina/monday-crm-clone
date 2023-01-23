2e7a3b5ad9404cf5b6667f506f9c58437d905e9f
Patrycja Miekina <patrycja.miekina@madetech.com>
2023-01-23 12:49:53 +0000

    add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Dashboard from './pages/Dashboard'
+import TicketPage from './pages/TicketPage'
+
 const App = () => {
   return (
     <div className="app">
-
+      {/* <Dashboard /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/ticket" element={<TicketPage />} />
+          <Route path="/ticket/:id" element={<TicketPage editMode={true} />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
